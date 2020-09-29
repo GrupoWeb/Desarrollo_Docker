52fde2888a711a532d9219e55523ff553e566545
Juan Jose Jolon <jjolong@miumg.edu.gt>
2020-09-29 15:26:16 -0600

    conexion

--- php/www/Control_De_Vehiculos/conexion/conexion.php
+++ php/www/Control_De_Vehiculos/conexion/conexion.php
@@ -1,6 +1,6 @@
 <?php
-    $serverName = "SistemaVehiculo\SQLEXPRESS";
-    // $serverName = "128.5.8.239";
+    // $serverName = "SistemaVehiculo\SQLEXPRESS";
+    $serverName = "128.5.8.239";
     $connectionInfo = array("Database" => "Control_Vehiculos", "UID"=>"sa", "PWD"=>"123456");
     $conn = sqlsrv_connect($serverName, $connectionInfo);
     if($conn === false)
